570034c80a0d715c1bb9fd1e495aff3586837eb2
ferrarifa <ferrarifabio600@gmail.com>
2026-06-16 14:31:21 -0300

    commit inicial

---
+++ src/main/java/com/monitoramento/ferramental/FerramentalApplication.java
@@ -0,0 +1,13 @@
+package com.monitoramento.ferramental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerramentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerramentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monitoramento/ferramental/FerramentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monitoramento.ferramental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerramentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
